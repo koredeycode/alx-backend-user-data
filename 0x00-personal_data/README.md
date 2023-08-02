fc95535db4033205ae39417e5ba69eb373efe3fa
koredeycode <koredey4u@gmail.com>
2023-08-02 19:22:47 +0000

    this

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,3 @@
+# 0x00-personal_data
+In this project, I learned about personal_data as part of my projects for the ALX Software Engineering Programme
+* Koredeycode
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+the filter_logger module containing the filter_datum function
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    return the log message obfuscated with the readacted string
+    """
+    pattern = r'({}=)[^{}]+'.format('=|'.join(fields), separator)
+    return re.sub(pattern, r'\1{}'.format(redaction), message)
