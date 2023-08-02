a893f404a722a439c2a51bac07000d6665988c7e
koredeycode <koredey4u@gmail.com>
2023-08-02 19:26:13 +0000

    function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,6 @@ from typing import List
 
 def filter_datum(fields: List, redaction: str,
                  message: str, separator: str) -> str:
-    """
-    return the log message obfuscated with the readacted string
-    """
+    """return the log message obfuscated with the readacted string"""
     pattern = r'({}=)[^{}]+'.format('=|'.join(fields), separator)
     return re.sub(pattern, r'\1{}'.format(redaction), message)
