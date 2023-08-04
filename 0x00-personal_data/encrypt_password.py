f76e37ab6687600b97a668ffbdd8701589f5b149
koredeycode <koredey4u@gmail.com>
2023-08-04 17:34:30 +0000

    done-with-password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     turn the password string argument to a salted, hashed password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    validate that the provided password matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
