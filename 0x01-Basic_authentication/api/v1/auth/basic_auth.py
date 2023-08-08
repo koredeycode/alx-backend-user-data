c3f0671b5e35a275c0e6847f6757e5b53459d273
koredeycode <koredey4u@gmail.com>
2023-08-08 15:19:07 +0000

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,3 +65,14 @@ class BasicAuth(Auth):
         if user is None or not user.is_valid_password(user_pwd):
             return None
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        retrieves the user instance for a request
+        """
+        header = self.authorization_header(request)
+        extract = self.extract_base64_authorization_header(header)
+        decoded_extract = self.decode_base64_authorization_header(extract)
+        email, pwd = self.extract_user_credentials(decoded_extract)
+        user = self.user_object_from_credentials(email, pwd)
+        return user
