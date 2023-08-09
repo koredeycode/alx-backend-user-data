63fa3c38702d1e58ce0e6ae723f4eb8ea4f74634
koredeycode <koredey4u@gmail.com>
2023-08-09 20:44:25 +0000

    done 100

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,6 +29,7 @@ if getenv('AUTH_TYPE') == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,37 +5,51 @@ Class to manage the SessionDB authentication
 
 from typing import TypeVar
 from api.v1.auth.session_exp_auth import SessionExpAuth
-from os import getenv
+from models.user_session import UserSession
+import uuid
+from datetime import datetime
 
 
 class SessionDBAuth(SessionExpAuth):
     """
     The Session Database Authentication class
     """
-    def __init__(self):
-        """
-        overload the init method
-        """
-        try:
-            self.session_duration = int(getenv('SESSION_DURATION'))
-            print(self.session_duration)
-        except Exception:
-            self.session_duration = 0
 
     def create_session(self, user_id=None):
         """
         overload the create_session method
         """
-        pass
+        session_id = str(uuid.uuid4())
+        session = UserSession(session_id=session_id, user_id=user_id)
+        session.save()
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """
         overload the user_id_for_session_id method
         """
-        pass
+        sessions = UserSession.search({"session_id": session_id})
+        if sessions is None or len(sessions) <= 0:
+            return None
+        session = sessions[0]
+        created_at = session.created_at
+        lapse = datetime.now() - created_at
+        if lapse.seconds > self.session_duration:
+            return None
+        return session.user_id
 
     def destroy_session(self, request=None):
         """
         overload the destroy_session method
         """
-        pass
+        if request is None:
+            return False
+        cookie = self.session_cookie(request)
+        if cookie is None:
+            return False
+        sessions = UserSession.search({'session_id': cookie})
+        if len(sessions) <= 0:
+            return False
+        session = sessions[0]
+        session.remove()
+        return True
