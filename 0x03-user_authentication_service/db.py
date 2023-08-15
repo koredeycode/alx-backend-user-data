292220cc7985d46ee8b54e3378f8cbe6d81d83bb
koredeycode <koredey4u@gmail.com>
2023-08-15 08:19:26 +0000

    this

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         add user to the database
         """
